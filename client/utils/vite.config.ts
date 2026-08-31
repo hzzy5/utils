5714b6eace6a8b9fcdcc4b8e27bb48c2322a0f08
lasse.effing <lasse.effing@study.hs-duesseldorf.de>
2026-08-31 23:08:45 +0200

    manifest setzen

--- client/utils/vite.config.ts
+++ client/utils/vite.config.ts
@@ -1,7 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "utils",
+        short_name: "utils",
+        display: "standalone",
+        theme_color: "#ffffff",
+        icons: [
+          { src: "./src/assets/utilslogo192.png", sizes: "192x192", type: "image/png" },
+          { src: "./src/assets/utilslogo512.png", sizes: "512x512", type: "image/png" }
+        ]
+      }
+    })
+  ]
+});
