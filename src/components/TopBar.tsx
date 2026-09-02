3106c5597956c513e22a3f0bd480cca3d3a06f22
lasse.effing <lasse.effing@study.hs-duesseldorf.de>
2026-09-02 13:48:19 +0200

    back button

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -13,11 +13,11 @@ export default function TopBar() {
           onClick={() => navigate(-1)}
           aria-label="Zurück"
           title="Zurück"
-          className="p-2 bg-white/90 dark:bg-black/80 text-sm rounded-md shadow-md hover:shadow-lg border flex items-center justify-center"
+          className=" text-sm rounded-md flex items-center justify-center"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-4 h-4"
+            className="w-8 h-8"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
